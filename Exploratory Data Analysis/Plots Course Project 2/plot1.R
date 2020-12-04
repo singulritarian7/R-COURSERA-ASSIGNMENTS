af5665c7cb487427a5300896cb24a854ca9e0829
Dipanshu Verma <72043963+dipanshuverma@users.noreply.github.com>
2020-12-04 16:44:25 +0530

    Add files via upload

---
+++ Exploratory Data Analysis/Plots Course Project 2/plot1.R
@@ -0,0 +1,31 @@
+archiveFile <- "NEI_data.zip"
+
+if(!file.exists(archiveFile)) {
+  
+  archiveURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+  
+  download.file(url=archiveURL,destfile=archiveFile)
+}
+
+if(!(file.exists("summarySCC_PM25.rds") && file.exists("Source_Classification_Code.rds"))) { 
+  unzip(archiveFile) 
+}
+
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Aggregate by sum the total emissions by year
+aggTotals <- aggregate(Emissions ~ year,NEI, sum)
+
+png("plot1.png",width=480,height=480,units="px",bg="transparent")
+
+barplot(
+  (aggTotals$Emissions)/10^6,
+  names.arg=aggTotals$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (10^6 Tons)",
+  main="Total PM2.5 Emissions From All US Sources"
+)
+
+dev.off()
